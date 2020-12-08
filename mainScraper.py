91779eef54b8c6a1374d184ea6396e303dbd2e1d
Stamy <ivan.t.stamatov@gmail.com>
2020-12-08 14:01:32 +0200

    Cleaned up and added more comments

--- mainScraper.py
+++ mainScraper.py
@@ -1,20 +1,3 @@
-# Plan:
-# go to moble.bg and search for subaru impreza (make an object for the search)
-# check how many pages there are and get the links for each page
-# go into the first page and get every listing
-# Export each listing into a database
-# Compare the listing to the existing ones in the database
-#     if it is the same, then do not add it
-# 
-# Database: SQLite
-# Web App: Flask
-# Need to check where to host it
-# 
-# 
-# 
-# 
-# 
-
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
@@ -31,33 +14,41 @@ indexInc0 = 0
 indexInc1 = 1 
 detailsDic = {}
 
+# Take input from terminal. This will be the request from the portal in the future
+# Have not done any formating of the result or any try/catch cases yet
 carBrand = input("Марка: ") 
 carModel = input("Модел: ") 
 carCategory = input("Категоря: ") 
 
+
+
+
+# ==== Section for only getting the link to the listings
+# Selenium - when creating the chrome browser, it will be invisible
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(executable_path = "drivers/chromedriver.exe", chrome_options=options)
 
+# Got to the home search page of mobile.bg
 driver.get('https://www.mobile.bg/pcgi/mobile.cgi?pubtype=1&act=2')
 
-# brand = select class="sw145", name="f5"
+# BeautifulSoup - brand = select class="sw145", name="f5"
 selectBrand = Select(driver.find_element_by_name('f5'))
 selectBrand.select_by_visible_text(carBrand)
 
-
-# model name = f6
+# BeautifulSoup - model name = f6
 selectModel = Select(driver.find_element_by_name('f6'))
 selectModel.select_by_visible_text(carModel)
 
-# category name = f14
+# BeautifulSoup - category name = f14
 selectCategory = Select(driver.find_element_by_name('f14'))
 selectCategory.select_by_visible_text(carCategory)
 
-# value = Т Ъ Р С И
+# BeautifulSoup - value = Т Ъ Р С И
 buttonSearch = driver.find_element_by_xpath("//input[@value='Т Ъ Р С И']")
 buttonSearch.click()
 
+# Get the current page as we need it to get all of the listings
 listingPage = driver.current_url
 print(listingPage)
 driver.quit()
@@ -65,24 +56,30 @@ driver.quit()
 
 
 
+
+# ==== Function for getting all car listing from the link we got from the first section
 def listingCollector(link):
+    # Beautiful Soup - opens the link and extracts the HTML from it
     newUrl = link
     uClient = uReq(newUrl)
     page_html = uClient.read()
     uClient.close()
     pageSoup = soup(page_html, "html.parser")
 
-
+    # Finds all A elements with the class "mmm". This depends on the site - we get it using Inspect Element and looking at the html code itself
     listingsListOld = pageSoup.find_all("a", class_="mmm")
-    # print("The number of listings are: " + str(len(listingsListOld)))
 
+    # For as many listings there are on the page, add each listing to a list
     for i in range(len(listingsListOld)):
         listingsList.append("https:" + listingsListOld[i].get('href'))
-    # print(listingsList)
     
     return
 
 
+
+
+
+# ==== Function for processing a listing and extracting details from it
 def listingData(linkPage):
     newUrl = linkPage
     uClient = uReq(newUrl)
@@ -90,11 +87,8 @@ def listingData(linkPage):
     uClient.close()
     pageSoup = soup(page_html, "html.parser")
 
-    
-
-
+    # Get the title of the listing 
     title = pageSoup.find_all('h1')[0].text
-    
     
 
     # span id="details_price"
@@ -102,6 +96,7 @@ def listingData(linkPage):
 
 
     # td style="line-height:24px; font-size:14px; color: #444;"
+    # Catching an error if there is no description
     try:
       description = pageSoup.find_all('td', attrs={'style' : 'line-height:24px; font-size:14px; color: #444;'})[0].text
       print(description)
@@ -110,10 +105,9 @@ def listingData(linkPage):
 
     # ul class="dilarData"
     detailsUL = pageSoup.find_all("ul", {"class": "dilarData"})[0]
-    
-
     detailsLI = detailsUL.find_all('li')
 
+    # A roundabout way of getting the UL/LI elements into basic text. When implementing a DB, this will change
     indexInc0 = 0
     indexInc1 = 1 
     detailsDict = {}
@@ -127,16 +121,14 @@ def listingData(linkPage):
 
     for key, value in detailsDict.items():
         stringUL += key + " : " +  value + "\n"
+    
 
 
-
+    # Print the collected data:
     print("Link to the web page: " + str(linkPage))
     print("Title of listing: " + title)
     print(price)
-    
     print(stringUL)
-
-
 
     return
 
@@ -144,35 +136,12 @@ def listingData(linkPage):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# url with a single page: https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=i0zolk&f1=1
-# url with 3 pages: https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=i0zjk1&f1=1
-
-uClient = uReq(listingPage) #downloads the webpages to a variable
+# ==== Main script section
+uClient = uReq(listingPage) 
 page_html = uClient.read()
 uClient.close()
 pageSoup = soup(page_html, "html.parser")
 
-
-
-
-# ===== Checking how many pages there are =====
-# the page ulr does not change besides the number at the end:
-# https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=i0ziek&f1=1
-# https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=i0ziek&f1=2
 # The first page will always be class="pageNumbersSelect"
 # Subsequent pages will be class="pageNumbers"
 #    So check how many instances there are of the class . The number of total pages = 1 + number of class instances
@@ -190,11 +159,12 @@ else:
         i += 1
     # print("The number of pages is : " + str(max(pageNumList)))
 
+    # Get the links for all pages and store them into a list
     mainPageLinks.append(listingPage)
     for i in range(max(pageNumList) - 1):
         mainPageLinks.append("https:" + numOfPages[i].get('href'))
 
-    # print(mainPageLinks)
+    # print(mainPageLinks) would look like:
     # {'page 1': 'https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=i0zjk1&f1=1', 'page 2': 'https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=i0zjk1&f1=2', 'page 3': 'https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=i0zjk1&f1=3'}  
 
 
@@ -202,28 +172,21 @@ else:
 # Going into every listing on every page
 # for every page:
 #   get all listings
-#       call function to go into every listing and gather data
-#           export data 
-
-# print(mainPageLinks)
-
 num = 0
-# for every page in the dic , call function to collect the listings
+# for every page in the list , call function to collect the listings
 for x in mainPageLinks:
-    # print("Current page is : " + mainPageLinks[num])
     listingCollector(mainPageLinks[num])
     num += 1
 
-# print("The number of listings are: " + str(len(listingsList)))
-# print(*listingsList, sep = "\n")
 
+
+# For each individual listing, call function to gather details from it
 num1 = 0
 for x in listingsList:
-    print("Listing number: " + str(num1 + 1))
+    # print("Listing number: " + str(num1 + 1))
     listingData(listingsList[num1])
     num1 += 1
 
-# listingData(listingsList[0])
 
 
-print("End of the script.")
+# print("End of the script.")
