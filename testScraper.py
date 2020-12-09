d46e4f516c88e51803ec6d5a519a68504931c73b
Ivan Ivanov <ivan.i.ivanov@egt-bg.com>
2020-12-09 16:52:55 +0200

    Added PUT request with brand name input.

--- testScraper.py
+++ testScraper.py
@@ -1,5 +1,5 @@
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 
 import requests
 
@@ -9,11 +9,20 @@ bs_objects = BeautifulSoup(html.read(), features='lxml')
 
 # find and extract form action url
 search_form = bs_objects.body.find('form', {'name':'search'})
-
 action_url = 'http:{}'.format(search_form['action'])
 
-# POST request to form
+# find all car brands form select element and put them in list
+brand_list = []
+for brand in  bs_objects.body.find('select', {'name':'marka'}).children:
+    if type(brand) is element.Tag:
+        brand_list.append( brand['value'] )
 
+
+user_input_marka = input('izberi marka: ')
+brand_idx = brand_list.index(user_input_marka)
+params = { 'marka': str(brand_idx) }
+
+
+# POST request to form with params
 post_request = requests.post(action_url)
-
 print(post_request.text)
